1c12df65c469208bbbc45d38462dada2e9b5d9dd
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-02 21:04:27 +0300

    Start setting bundler

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # To-Do List
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/main.ts",
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.tsx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.runtime.esm.js",
+      "@": path.resolve(__dirname, "src"),
+    },
+    extensions: [".ts", ".js", ".vue", ".json"],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: "To-Do-List",
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
