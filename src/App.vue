78b911dddb214d941e35f5f9ebfb54893b70f947
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-04 17:38:15 +0300

    Update bundler - eslint, babel

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <h3>{{ defineAutorName }}</h3>
   </div>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        title: "Hello",
-      };
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      title: "Hello",
+      author: "Dima",
+    };
+  },
+  computed: {
+    defineAutorName(): string {
+      return this.author ? this.author : "unknown";
     },
-  };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Fuck";
+    },
+  },
+});
 </script>
 
 <style lang="scss">
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
+const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -17,6 +19,14 @@ module.exports = merge(common, {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public/assets"),
+          noErrorOnMissing: true,
+        },
+      ],
     }),
   ],
   optimization: {
