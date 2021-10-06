e9d57ec2922df5d22f382e7f796610e947eb18a9
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-06 14:09:46 +0300

    Uncomment overrides block in eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,15 +19,12 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
-  // overrides: [
-  //   {
-  //     files: [
-  //       "**/__tests__/*.{j,t}s?(x)",
-  //       "**/tests/unit/**/*.spec.{j,t}s?(x)",
-  //     ],
-  //     env: {
-  //       jest: true,
-  //     },
-  //   },
-  // ],
+  overrides: [
+    {
+      files: ["**/__tests__/*.ts", "**/tests/unit/**/*.spec.ts"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
