6af148d9a85f2d63788cf30cf35b5ee4c429d511
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-06 18:00:46 +0300

    Add material componets md-list, md-icon

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,21 @@ import Vue from "vue";
 
 import App from "./App.vue";
 
-import MdButton from 'vue-material/dist/components/MdButton';
-import MdField from 'vue-material/dist/components/MdField';
-import MdRadio from 'vue-material/dist/components/MdRadio';
-import MdCheckbox from 'vue-material/dist/components/MdCheckbox';
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import MdButton from "vue-material/dist/components/MdButton";
+import MdField from "vue-material/dist/components/MdField";
+import MdRadio from "vue-material/dist/components/MdRadio";
+import MdCheckbox from "vue-material/dist/components/MdCheckbox";
+import MdList from "vue-material/dist/components/MdList";
+import MdIcon from "vue-material/dist/components/MdIcon";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
 
-Vue.use(MdButton)
-Vue.use(MdCheckbox)
-Vue.use(MdField)
-Vue.use(MdRadio)
+Vue.use(MdButton);
+Vue.use(MdCheckbox);
+Vue.use(MdField);
+Vue.use(MdRadio);
+Vue.use(MdList);
+Vue.use(MdIcon);
 
 Vue.config.productionTip = false;
 
