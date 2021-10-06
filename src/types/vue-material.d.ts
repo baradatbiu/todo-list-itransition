890fa4ebec4ec00640371556fd62ab52cab99432
Kharlamov Dmitry <newdimaak@gmail.com>
2021-10-06 18:00:46 +0300

    Add material componets md-list, md-icon

--- src/types/vue-material.d.ts
+++ src/types/vue-material.d.ts
@@ -1,4 +1,6 @@
-declare module 'vue-material/dist/components/MdButton';
-declare module 'vue-material/dist/components/MdCheckbox';
-declare module 'vue-material/dist/components/MdField';
-declare module 'vue-material/dist/components/MdRadio';
+declare module "vue-material/dist/components/MdButton";
+declare module "vue-material/dist/components/MdCheckbox";
+declare module "vue-material/dist/components/MdField";
+declare module "vue-material/dist/components/MdRadio";
+declare module "vue-material/dist/components/MdList";
+declare module "vue-material/dist/components/MdIcon";
